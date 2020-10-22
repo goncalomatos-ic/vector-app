bbb79068ef039404c83c068d1db6b1b1754621f1
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-22 23:54:12 +0100

    Dnd WIP

--- src/components/CardRow.js
+++ src/components/CardRow.js
@@ -1,20 +1,81 @@
 import React from 'react';
 import Card from './Card';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  display: 'flex',
+  padding: 8,
+  overflow: 'auto',
+});
 
 class CardsRow extends React.Component {
-  render() {
-    const items = []
-
-    // Sort cards by the position attribute and split cards rows every 3 elements
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [],
+    };
     for (var index in this.props.cards) {
       let card = this.props.cards[index]
-      items.push(<Card key={card.position.toString()} info={card}/>)
+      this.state.cards.push(<Card key={card.position.toString()} info={card}/>)
+    }
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
+
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
     }
 
+    const cards = reorder(
+      this.state.cards,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      cards,
+    });
+  }
+
+  render() {
     return (
       <div className="CardRow">
-        {items}
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId="droppable" direction="horizontal">
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+                {...provided.droppableProps}
+              >
+                {this.state.cards.map((item, index) => (
+                  <Draggable key={`item-${index}`} draggableId={`item-${index}`} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}>
+                        {item}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
       </div>
+
     );
   }
 }
