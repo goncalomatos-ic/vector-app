87d298d588d4fba2e22d4ad1918f425841d2df7a
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-22 20:13:07 +0100

    Create card rows of 3 elements

---
+++ src/components/CardRow.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Card from './Card';
+
+class CardsRow extends React.Component {
+  render() {
+    const items = []
+
+    // Sort cards by the position attribute and split cards rows every 3 elements
+    for (var index in this.props.cards) {
+      let card = this.props.cards[index]
+      items.push(<Card key={card.position.toString()} info={card}/>)
+    }
+
+    return (
+      <div>
+        {items}
+      </div>
+    );
+  }
+}
+
+export default CardsRow;
--- src/components/CardsList.js
+++ src/components/CardsList.js
@@ -1,21 +1,35 @@
 import React from 'react';
-import Card from './Card'
+import CardRow from './CardRow';
 
 class CardsList extends React.Component {
-  render() {
+  getCardRows(){
     const items = []
-    const json_content = JSON.parse(this.props.content)
+    const json_content = JSON.parse(this.props.content).sort(function (a, b) {return a.position - b.position;});
+    let cards = []
 
-    for (var key in json_content) {
-      items.push(<Card />)
+    // Sort cards by the position attribute and split cards rows every 3 elements
+    for (var index in json_content) {
+      cards.push(json_content[index])
+      if (cards.length === 3 && index > 0) {
+        items.push(<CardRow key={index.toString()} cards={cards}/>)
+        cards = []
+      }
     }
 
+    if (cards.length > 0){
+      items.push(<CardRow key={index.toString()} cards={cards}/>)
+    }
+
+    return items
+  }
+
+  render() {
     return (
       <div>
-        {items}
+        {this.getCardRows()}
       </div>
     );
-    }
+  }
 }
 
 export default CardsList;
