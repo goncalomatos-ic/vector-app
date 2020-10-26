5be8f58dcc87082c37bb5c837014621d98f719e8
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-26 11:02:13 +0000

    Read cards info from api

--- src/components/App.js
+++ src/components/App.js
@@ -4,24 +4,43 @@ import InputJson from './InputJson'
 import CardsList from './CardsList'
 
 class App extends React.Component {
-  getContent(){
-    return JSON.stringify([
-      {"type": "bank-draft", "title": "Bank Draft", "position": 0},
-      {"type": "bill-of-lading", "title": "Bill of Lading","position": 1 },
-      {"type": "invoice", "title": "Invoice","position": 2},
-      {"type": "bank-draft-2", "title": "Bank Draft2", "position": 3},
-      {"type": "bill-of-lading-2", "title":"Bill of Lading 2", "position": 4}
-    ], null, 2)
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      content: JSON.stringify([])
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/cards")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(JSON.stringify(result))
+          this.setState({
+            isLoaded: true,
+            content: JSON.stringify(result)
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
   render() {
     return (
       <div className="App">
         <div className="inputJsonDiv">
-          <InputJson content={this.getContent()}/>
+          <InputJson content={this.state.content}/>
         </div>
         <div className="imageRowsDiv">
-          <CardsList content={this.getContent()}/>
+          <CardsList content={this.state.content}/>
         </div>
       </div>
     );
