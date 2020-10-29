4fb9dd0b706d4a29bd3fe559e3c3ca37febfdbce
Gonçalo <goncalo.matos@indiecampers.com>
2020-10-29 09:24:15 +0000

    test

--- src/components/App.js
+++ src/components/App.js
@@ -17,7 +17,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-    axios.get(`${process.env.API_ENDPOINT}/cards`)
+    axios.get(`http://127.0.0.1:8000/cards`)
     .then(res => {
       document.getElementById("inputJsonTextArea").value = JSON.stringify(res.data)
       this.setState({
@@ -31,7 +31,7 @@ class App extends React.Component {
     let data = document.getElementById("inputJsonTextArea").value
     data = JSON.parse(data.replace(/\s/g, ""))
 
-    axios.patch(`${process.env.API_ENDPOINT}/cards`, { data })
+    axios.patch(`http://127.0.0.1:8000/cards`, { data })
     .then(res => {
         this.setState({
           isLoaded: true,
